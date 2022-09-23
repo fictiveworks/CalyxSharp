3ce4f6752b66266502680f9ebc8a51dbebe16ab4
Ben Torkington <prmera@gmail.com>
2022-09-23 13:16:10 +1200

    move Cycle class to its own file

--- CalyxSharp/Registry.cs
+++ CalyxSharp/Registry.cs
@@ -69,7 +69,8 @@ namespace Calyx
     {
       // If this symbol has not been expanded as a cycle then register it
       if (!cycles.ContainsKey(symbol)) {
-        int cycleLength = Expand(symbol).Length;
+        var prod = Expand(symbol);
+        int cycleLength = prod.Length;
         cycles.Add(symbol, new Cycle(options, cycleLength));
       }
 
@@ -103,42 +104,6 @@ namespace Calyx
       context = new Dictionary<string, Rule>();
       memos = new Dictionary<string, Expansion>();
       cycles = new Dictionary<string, Cycle>();
-    }
-  }
-
-  public class Cycle
-  {
-    Options options;
-    int index;
-    List<int> sequence;
-
-    public Cycle(Options options, int count)
-    {
-      if (count < 1) {
-        throw new ArgumentException("'count' must be greater than zero");
-      }
-      this.options = options;
-      index = 0;
-      sequence = Enumerable.Range(0, count).ToList();
-      Shuffle();
-    }
-
-    public void Shuffle()
-    {
-      int current = sequence.Count;
-      while (current > 1) {
-        current--;
-        int target = options.Rng.Next(current + 1);
-        int swap = sequence[target];
-        sequence[target] = sequence[current];
-        sequence[current] = swap;
-      }
-    }
-
-    public int Poll()
-    {
-      // TODO: repeat ad infinitum or reshuffle each time final index is polled?
-      return sequence[index++ % sequence.Count];
     }
   }
 }

48bcf194d8a719fb809ff247d0107abc7c7032fe
Ben Torkington <prmera@gmail.com>
2022-09-24 12:09:50 +1200

    implement WeightedBranch

--- CalyxSharp/Production/WeightedBranch.cs
+++ CalyxSharp/Production/WeightedBranch.cs
@@ -1,10 +1,81 @@
+using Calyx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Calyx.Production
 {
   public class WeightedBranch
   {
-    // public Expansion Evaluate(Options options)
-    // {
-    //
-    // }
+    /* if this constructor is ever made public, hoist the bounds checks from the
+     * Parse(Dictionary<string, double>, Registry) convenience init into the constructor */
+    private WeightedBranch(WeightedProduction[] productions, Registry registry)
+    {
+      this.productions = productions;
+      this.registry = registry;
+      if (productions.Any(p => p.Weight <= 0))
+      {
+        throw new ArgumentOutOfRangeException("All weights must be greater than zero");
+      }
+      sumOfWeights = productions.Sum(production => production.Weight);
+    }
+
+    private WeightedProduction[] productions;
+    private Registry registry;
+    private double sumOfWeights;
+
+    public Expansion Evaluate(Options options)
+    {
+      double max = sumOfWeights;
+      double waterMark = options.Rng.NextDouble() * sumOfWeights;
+      WeightedProduction production = productions.FirstOrDefault(wp => waterMark >= (max -= wp.Weight));
+      return new Expansion(Exp.WeightedBranch, production.Production.Evaluate(options));
+    }
+
+    /* Until C# 7, which will allow generic types to constrained to `INumber`, we'll support
+     * the different numeric types by providing a convenience init for each of them */
+
+    public static WeightedBranch Parse(Dictionary<string, int> choices, Registry registry)
+    {
+      WeightedProduction[] weightedProductions = choices.Select(choice => new WeightedProduction { 
+        Production = Syntax.TemplateNode.Parse(choice.Key, registry),
+        Weight = choice.Value,
+      }).ToArray();
+      return new WeightedBranch(weightedProductions, registry);
+    }
+
+    public static WeightedBranch Parse(Dictionary<string, double> choices, Registry registry)
+    {
+      if (choices.Any(p => double.IsInfinity(p.Value)))
+      {
+        throw new ArgumentOutOfRangeException("Weights may not be infinite");
+      }
+      if (choices.Any(p => double.IsNaN(p.Value)))
+      {
+        throw new ArgumentException("Weights may not be NaN");
+      }
+
+      WeightedProduction[] weightedProductions = choices.Select(choice => new WeightedProduction
+      {
+        Production = Syntax.TemplateNode.Parse(choice.Key, registry),
+        Weight = choice.Value,
+      }).ToArray();
+      return new WeightedBranch(weightedProductions, registry);
+    }
+
+    public static WeightedBranch Parse(Dictionary<string, decimal> choices, Registry registry)
+    {
+      WeightedProduction[] weightedProductions = choices.Select(choice => new WeightedProduction
+      {
+        Production = Syntax.TemplateNode.Parse(choice.Key, registry),
+        Weight = (double)choice.Value,
+      }).ToArray();
+      return new WeightedBranch(weightedProductions, registry);
+    }
+    internal struct WeightedProduction
+    {
+      public double Weight;
+      public IProduction Production;
+    }
   }
 }
---
+++ CalyxSharpTests/Production/WeightedBranchTest.cs
@@ -0,0 +1,118 @@
+using Calyx.Production;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calyx.Test.Production
+{
+  internal class WeightedBranchTest
+  {
+    [Test]
+    public void BranchWithMultiChoiceTest()
+    {
+      WeightedBranch branch = WeightedBranch.Parse(new System.Collections.Generic.Dictionary<string, int> {
+        { "lithium", 50 },
+        { "silicon", 30 },
+        { "carbon", 20 },
+      }, new Registry());
+
+      Expansion exp = branch.Evaluate(new Options(seed: 1234));
+
+      Assert.That(exp.Symbol, Is.EqualTo(Exp.WeightedBranch));
+      Assert.That(exp.Tail[0].Symbol, Is.EqualTo(Exp.Template));
+      Assert.That(exp.Tail[0].Tail[0].Symbol, Is.EqualTo(Exp.Atom));
+      Assert.That(exp.Tail[0].Tail[0].Term, Is.EqualTo("silicon"));
+    }
+
+    [Test]
+    public void BranchWithDoubleWeights()
+    {
+      WeightedBranch branch = WeightedBranch.Parse(new System.Collections.Generic.Dictionary<string, double> {
+        { "lithium", 0.5 },
+        { "silicon", 0.3 },
+        { "carbon", 0.2 },
+      }, new Registry());
+
+      Expansion exp = branch.Evaluate(new Options(seed: 1234));
+
+      Assert.That(exp.Symbol, Is.EqualTo(Exp.WeightedBranch));
+      Assert.That(exp.Tail[0].Symbol, Is.EqualTo(Exp.Template));
+      Assert.That(exp.Tail[0].Tail[0].Symbol, Is.EqualTo(Exp.Atom));
+      Assert.That(exp.Tail[0].Tail[0].Term, Is.EqualTo("silicon"));
+    }
+
+    [Test]
+    public void BranchWithDecimalWeights()
+    {
+      WeightedBranch branch = WeightedBranch.Parse(new System.Collections.Generic.Dictionary<string, decimal> {
+        { "lithium", 0.5m },
+        { "silicon", 0.3m },
+        { "carbon", 0.2m },
+      }, new Registry());
+
+      Expansion exp = branch.Evaluate(new Options(seed: 1234));
+
+      Assert.That(exp.Symbol, Is.EqualTo(Exp.WeightedBranch));
+      Assert.That(exp.Tail[0].Symbol, Is.EqualTo(Exp.Template));
+      Assert.That(exp.Tail[0].Tail[0].Symbol, Is.EqualTo(Exp.Atom));
+      Assert.That(exp.Tail[0].Tail[0].Term, Is.EqualTo("silicon"));
+    }
+
+    [Test]
+    public void ResultsRoughlyMatchWeightings()
+    {
+      int fizzTimes = 700;
+      int buzzTimes = 300;
+
+      WeightedBranch branch = WeightedBranch.Parse(new System.Collections.Generic.Dictionary<string, int> {
+        { "fizz", fizzTimes },
+        { "buzz", buzzTimes },
+      },new Registry());
+
+      Expansion[] res = Enumerable.Range(0, fizzTimes + buzzTimes).Select(n => branch.Evaluate(new Options(seed: n))).ToArray();
+
+      int totalFizz = res.Where(exp => exp.Tail[0].Tail[0].Term.Equals("fizz")).Count();
+      int totalBuzz = res.Where(exp => exp.Tail[0].Tail[0].Term.Equals("buzz")).Count();
+
+      Assert.That(totalFizz, Is.EqualTo(fizzTimes).Within(1).Percent);
+      Assert.That(totalBuzz, Is.EqualTo(buzzTimes).Within(1).Percent);
+    }
+
+    [TestCase(0)]
+    [TestCase(-1)]
+    [TestCase(int.MinValue)]
+    [Test]
+    public void NonPositiveWeightsAreRejected(int testCase)
+    {
+      Dictionary<string, int> choices = new Dictionary<string, int> {
+        { "yin", 10 },
+        { "yang", testCase },
+      };
+
+      Assert.Throws<ArgumentOutOfRangeException>(() => WeightedBranch.Parse(choices, new Registry()));
+    }
+
+    [Test]
+    public void InfiniteWeightsAreRejected()
+    {
+      Dictionary<string, double> choices = new Dictionary<string, double> {
+        { "yin", 1.0 },
+        { "yang", double.PositiveInfinity },
+      };
+
+      Assert.Throws<ArgumentOutOfRangeException>(() => WeightedBranch.Parse(choices, new Registry()));
+    }
+
+    [Test]
+    public void NotANumberWeightsAreRejected()
+    {
+      Dictionary<string, double> choices = new Dictionary<string, double> {
+        { "yin", 1.0 },
+        { "yang", double.NaN },
+      };
+
+      Assert.Throws<ArgumentException>(() => WeightedBranch.Parse(choices, new Registry()));
+    }
+  }
+}

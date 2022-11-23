7ed7a53482061c3c2e83eaee819fd4db96de7748
Mark Rickerby <me@maetl.net>
2022-11-23 23:48:27 +1300

    Bump version to 0.4

--- Calyx/AssembyInfo.cs
+++ Calyx/AssembyInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("Calyx")]
 [assembly: AssemblyCopyright("Copyright © 2021 Fictiveworks")]
-[assembly: AssemblyVersionAttribute("0.3.0.0")]
+[assembly: AssemblyVersionAttribute("0.4.0.0")]
